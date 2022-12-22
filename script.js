9a4d1fc4c0dfc6f0ddeba81e54973f2d24ce0533
Aladser <aladser@gmail.com>
2022-12-22 17:30:55 +0900

    добавление перемешивания

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const sortingMethods = ['bubbleSort','insertSort','selectionSort','mergeSort','quickSort'];
+const sortHeaders = ['Пузырьковая сортировка','Сортировка вставками','Сортировка выбором','Сортировка слиянием','Быстрая сортировка'];
+const sortlistOutput = document.querySelectorAll('.sortlist');
+const btnSort = document.querySelector('.btn-sort');
+let sortType = SortAPI.INCREASE;
+
+/** генерация случайного целого числа */
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+/** список игр */
 let games = [
     {name:"Tropico 6", time: 130},
     {name:"Skyrim", time: 155},
@@ -7,18 +17,12 @@ let games = [
     {name:"Grim Dawn", time: 58},
 ];
 
-let sortType = SortAPI.INCREASE;
-const sortCollection = [
-    {name:'Оригинальный список', gamesList: games},
-    {name:'Пузырьковая сортировка', gamesList: SortAPI.bubbleSort(games, 'time', sortType)},
-    {name:'Сортировка вставками', gamesList: SortAPI.insertSort(games, 'time', sortType)},
-    {name:'Сортировка выбором', gamesList: SortAPI.selectionSort(games, 'time', sortType)},
-    {name:'Сортировка слиянием', gamesList: SortAPI.mergeSort(games, 'time', sortType)},
-    {name:'Быстрая сортировка', gamesList: SortAPI.quickSort(games, 'time', sortType)}
-];
+let arrays = [];
+arrays[0] = games;
 
-function printList(output, arr){
-    arr.forEach(elem => {
+/** добавить список игр */
+function addSortlistContent(output, arr){
+    arr.forEach(function(elem){
        let elemDiv = document.createElement('div');
        elemDiv.className = "sortlist__elem sortlist__content";
 
@@ -32,10 +36,60 @@ function printList(output, arr){
        output.appendChild(elemDiv);
     });
 }
-
-let sortlists = document.querySelectorAll('.sortlist');
-for(let i=0; i<sortCollection.length; i++){
-    printList(sortlists[i], sortCollection[i].gamesList);
+/** удалить список игр*/
+function deleteSortlistContent(output){
+    output.querySelectorAll('.sortlist__content').forEach(elem => elem.remove());
 }
 
+/** создать сортированные массивы */
+function sortData(){
+    sortType = sortType==SortAPI.INCREASE ? SortAPI.DESCENDING : SortAPI.INCREASE;
+    for(let i=0; i<sortingMethods.length; i++){
+        arrays[i+1] = SortAPI[sortingMethods[i]](games, 'time', sortType);
+    }
+}
+/** Удалить сортированные массивы*/
+function clearData(){
+    for(let i = 1; i<sortlistOutput.length; i++){
+        deleteSortlistContent(sortlistOutput[i]);
+    }
+}
 
+/** вывести сортированные массивы */
+function display(){
+    for(let i=0; i<sortingMethods.length; i++){
+        sortlistOutput[i+1].querySelector('.sortlist__header').textContent = sortHeaders[i];
+        addSortlistContent(sortlistOutput[i+1], arrays[i+1]);
+    }
+}
+
+addSortlistContent(sortlistOutput[0], games);
+sortData();
+display();
+
+function shuffleArray(_arr){
+    let arr = _arr.slice(0);
+    let rslt = [];
+    let index;
+    while(arr.length > 0){
+      index = getRandomInt(0, arr.length-1);
+      rslt.push(arr[index]);
+      arr.splice(index, 1);
+    }
+    return rslt;
+}
+
+// возрастание - \u{2191}; убывание - \u{2193}
+btnSort.addEventListener('click', function(){   
+    this.value = sortType==SortAPI.INCREASE ? "\u{2191}" : "\u{2193}";
+    sortData();
+    clearData();
+    display();
+})
+
+document.querySelector('.btn-shuffle').addEventListener('click', function(){
+    btnSort.value = 'Сортировать';
+    for(let i=1; i<sortlistOutput.length; i++) arrays[i] = shuffleArray(arrays[i]);
+    clearData();
+    display();
+});
